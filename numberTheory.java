6758308d8847a2ebabbfcf00aa0853fc0ec61e67
Anupam <anupam38b@gmail.com>
2021-10-05 02:02:05 +0530

    update

--- numberTheory.java
+++ numberTheory.java
@@ -3,4 +3,14 @@ public class numberTheory{
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
     }
+
+    // GCD
+    public int gcd(int a, int b){
+        return  b == 0 ?  a : gcd(b, a%b);     // REM : alternate b,a,b,a,b 
+    }
+
+    // LCM 
+    public int lcm(int a, int b){
+        return (a*b)/gcd(a, b);
+    }
 }
